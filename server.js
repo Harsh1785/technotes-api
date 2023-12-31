ea5c0bcd19f3af03caf9b50cf80d6e9b3a1e73ac
Harsh1785 <112305031+Harsh1785@users.noreply.github.com>
2023-12-31 14:11:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const PORT = process.env.PORT || 3500
 
 console.log(process.env.NODE_ENV)
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://technotes-gqva.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 connectDB()
 
 app.use(logger)
